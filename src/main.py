46cb1fff0f1d5286c9a8fa986f3171ee48f967bc
Lokyi He <lokyi.he@student.unsw.edu.au>
2026-08-28 19:28:58 +1000

    changed function name

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from deposit import deposit
 from bet import get_bet
-from bet_lines import get_num_lines
+from bet_lines import get_bet_lines
 from spin import spin
 
 def main():
@@ -10,7 +10,7 @@ def main():
     print(f"Current balance is {balance}")
     input("Press any key to start game (ctrl d to quit)")
     bet_amount = get_bet(balance)
-    list_of_lines, balance, total_bet_amount = get_num_lines(balance, bet_amount)
+    list_of_lines, balance, total_bet_amount = get_bet_lines(balance, bet_amount)
     balance = spin(list_of_lines, balance, bet_amount, total_bet_amount)
     if balance == 0:
       print(f"Your balance is $0")
